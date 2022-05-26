a211b12fcef76546f811ad45a1bf054201e4745d
CarvalhoDeLucas <lucas.edyardo@gmail.com>
2022-05-26 14:28:06 -0300

    GetMapping feito para clientes

--- vendas_restful/src/main/java/io/curso/vendas/VendasApplication.java
+++ vendas_restful/src/main/java/io/curso/vendas/VendasApplication.java
@@ -1,10 +1,23 @@
 package io.curso.vendas;
 
+import io.curso.vendas.domain.entity.Cliente;
+import io.curso.vendas.domain.repository.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VendasApplication {
+
+    @Bean
+    public CommandLineRunner commandLineRunner(@Autowired Clientes clientes) {
+        return args -> {
+            Cliente cliente = new Cliente(null, "Lucas");
+            clientes.save(cliente);
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
--- vendas_restful/src/main/java/io/curso/vendas/rest/controller/ClienteController.java
+++ vendas_restful/src/main/java/io/curso/vendas/rest/controller/ClienteController.java
@@ -1,19 +1,43 @@
 package io.curso.vendas.rest.controller;
 
+import io.curso.vendas.domain.entity.Cliente;
+import io.curso.vendas.domain.repository.Clientes;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/api/clientes")
 public class ClienteController {
 
-    @RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET)
+    private Clientes clientes;
+
+    public ClienteController(Clientes clientes) {
+        this.clientes = clientes;
+    }
+
+    // Exemplo de @RequestMapping
+    @RequestMapping(
+            value = "/hello/{nome}",
+            method = RequestMethod.POST,
+            consumes = {"Application/json", "Application/xml"},
+            produces = {"Application/json", "Application/xml"}
+    )
     @ResponseBody
-    public String helloCliente(@PathVariable("nome") String nomeCliente) {
+    public String helloCliente(@PathVariable("nome") String nomeCliente, @RequestBody Cliente cliente) {
         return String.format("Hello %s", nomeCliente);
     }
 
+    @GetMapping("/{id}")
+    @ResponseBody
+    public ResponseEntity<Cliente> getClienteById(@PathVariable Integer id) {
+        Optional<Cliente> cliente = clientes.findById(id);
+
+        return cliente
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build()
+        );
+    }
 }
