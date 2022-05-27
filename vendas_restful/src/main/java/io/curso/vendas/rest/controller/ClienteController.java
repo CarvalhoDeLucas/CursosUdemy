69cf790bd5ccc99ee62096a5fca910b279b6b606
CarvalhoDeLucas <lucas.edyardo@gmail.com>
2022-05-27 11:24:32 -0300

    PutMapping e GetMapping (com example para realizar o filtro)

--- vendas_restful/src/main/java/io/curso/vendas/rest/controller/ClienteController.java
+++ vendas_restful/src/main/java/io/curso/vendas/rest/controller/ClienteController.java
@@ -2,10 +2,13 @@ package io.curso.vendas.rest.controller;
 
 import io.curso.vendas.domain.entity.Cliente;
 import io.curso.vendas.domain.repository.Clientes;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -35,7 +38,12 @@ public class ClienteController {
     public ResponseEntity<Cliente> getClienteById(@PathVariable Integer id) {
         Optional<Cliente> cliente = clientes.findById(id);
 
-        return cliente.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+        if (cliente.isPresent()) {
+            return ResponseEntity.ok(cliente.get());
+        }
+
+        return ResponseEntity.notFound().build();
+        // return cliente.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping("/")
@@ -43,5 +51,44 @@ public class ClienteController {
     public ResponseEntity<Cliente> save(@RequestBody Cliente cliente) {
         Cliente clienteSalvo = clientes.save(cliente);
         return ResponseEntity.ok(clienteSalvo);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseBody
+    public ResponseEntity delete(@PathVariable Integer id) {
+        Optional<Cliente> cliente = clientes.findById(id);
+
+        if (cliente.isPresent()) {
+            clientes.delete(cliente.get());
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping("/{id}")
+    @ResponseBody
+    public ResponseEntity update(@PathVariable Integer id, @RequestBody Cliente cliente) {
+        return clientes
+                .findById(id)
+                .map(clienteExiste -> {
+                    cliente.setId(clienteExiste.getId());
+                    clientes.save(cliente);
+                    return ResponseEntity.noContent().build();
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/")
+    public ResponseEntity find(Cliente filtro) {
+        ExampleMatcher matcher = ExampleMatcher
+                                    .matching()
+                                    .withIgnoreCase()
+                                    .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING);
+
+        Example example = Example.of(filtro, matcher);
+        List<Cliente> list = clientes.findAll(example);
+
+        return ResponseEntity.ok(list);
     }
 }
