3ff9ab76579b3467354ae6f80e3020a0530e243c
CarvalhoDeLucas <lucas.edyardo@gmail.com>
2022-05-30 07:15:54 -0300

    Erro de exceptions

---
+++ vendas_restful/src/main/java/io/curso/vendas/rest/controller/ApplicationControllerAdvice.java
@@ -0,0 +1,20 @@
+package io.curso.vendas.rest.controller;
+
+import io.curso.vendas.exception.RegraNegocioException;
+import io.curso.vendas.rest.ApiErrors;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(RegraNegocioException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErrors handleRegraNegocioException(RegraNegocioException ex) {
+        String messageError = ex.getMessage();
+        return new ApiErrors((messageError));
+    }
+
+}
