4af22de30ee40e24e02a39f6e96e76a2716d9bd5
CarvalhoDeLucas <lucas.edyardo@gmail.com>
2022-05-31 18:28:04 -0300

    Iniciando o Bean Validation

--- vendas_restful/src/main/java/io/curso/vendas/rest/controller/ApplicationControllerAdvice.java
+++ vendas_restful/src/main/java/io/curso/vendas/rest/controller/ApplicationControllerAdvice.java
@@ -3,24 +3,40 @@ package io.curso.vendas.rest.controller;
 import io.curso.vendas.exception.PedidoNaoEncontratoException;
 import io.curso.vendas.exception.RegraNegocioException;
 import io.curso.vendas.rest.ApiErrors;
-import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.http.HttpStatus.*;
 
 @RestControllerAdvice
 public class ApplicationControllerAdvice {
 
     @ExceptionHandler(RegraNegocioException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseStatus(BAD_REQUEST)
     public ApiErrors handleRegraNegocioException(RegraNegocioException ex) {
         return new ApiErrors(ex.getMessage());
     }
 
     @ExceptionHandler(PedidoNaoEncontratoException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseStatus(NOT_FOUND)
     public ApiErrors handlePedidoNaoEncontrado(PedidoNaoEncontratoException ex) {
-        return new ApiErrors( ex.getMessage());
+        return new ApiErrors(ex.getMessage());
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(BAD_REQUEST)
+    public ApiErrors handleMethodNotValidException(MethodArgumentNotValidException ex) {
+        List<String> errors = ex.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(error -> error.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        return new ApiErrors(errors);
+    }
 }
