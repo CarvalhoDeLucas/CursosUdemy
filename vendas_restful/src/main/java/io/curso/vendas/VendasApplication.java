17e1618382448a2f37b5b4959aa16f18ba8e3c39
CarvalhoDeLucas <lucas.edyardo@gmail.com>
2022-06-04 18:23:34 -0300

    Finalizando documentação

--- vendas_restful/src/main/java/io/curso/vendas/VendasApplication.java
+++ vendas_restful/src/main/java/io/curso/vendas/VendasApplication.java
@@ -1,15 +1,11 @@
 package io.curso.vendas;
 
-import io.curso.vendas.domain.entity.Cliente;
-import io.curso.vendas.domain.repository.Clientes;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class VendasApplication {
+public class VendasApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
